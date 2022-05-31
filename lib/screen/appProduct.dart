f8d6cadcccef590f44ba08ada4cdb31dc2bef574
Final final <finalp58@gmail.com>
2022-05-31 16:50:27 +0700

    up

--- lib/screen/appProduct.dart
+++ lib/screen/appProduct.dart
@@ -15,18 +15,45 @@ class AddProduct extends StatefulWidget {
   @override
   State<AddProduct> createState() => _AddProductState();
 }
+
 class _AddProductState extends State<AddProduct> {
+  GlobalKey<FormState> Key = GlobalKey<FormState>();
   Product_data Product = Product_data();
   XFile? images;
   double neenear = 55;
   String? position;
-void set(){
-  setState(() {
-    images = image;
-  });
-  
-}
 
+  void set() {
+    setState(() {
+      images = image;
+    });
+  }
+
+  ceck() {
+    if (Key.currentState!.validate()) {
+      Key.currentState!.save();
+    }
+  }
+
+  elevatedButton() {
+    return Container(
+      width: 390,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            primary: element.main,
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16)),
+        onPressed: () {
+          uptostorge('dddd', 'desciption', 10, 10, 10, 'category');
+          ceck();
+        },
+        child: const Text(
+          "ບັນທືກ",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,166 +63,167 @@ void set(){
         leading: element.backpage(context),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 32,
-            ),
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  addimgae();
-                  Timer(
-                    Duration(seconds: 2),
-                      ()=>set()
-                  );
-                  neenear = 100;
-                },
-                child: CircleAvatar(
-                  radius: neenear ,
-                  backgroundColor: element.main,
-                  child: images != null
-                      ? ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.file(
-                            File(images!.path),
-                            width: 200,
-                            height: 200,
-                            fit: BoxFit.cover,
-                          ),
-                        )
-                      : Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(50)),
-                          width: 102,
-                          height: 102,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.camera_alt,
-                                color: Colors.grey[800],
-                              ),
-                              Text('ຮູບພາບ',style: TextStyle(color: Colors.grey[800],)),
-                            ],
-                          ),
-                        ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            _inputFields(context)
-          ],
-        ),
+        child: buildColumn(context),
       ),
     );
   }
 
-}
-
-_inputFields(context) {
-  return Padding(
-    padding: const EdgeInsets.only(right: 20, left: 20),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        TextField(
-          decoration: InputDecoration(
-            hintText: "ຊື່ສິນຄ້າ",
-            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
-            filled: true,
-            prefixIcon: const Icon(Icons.shopping_cart_outlined),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
-              borderSide: BorderSide.none,
+  Column buildColumn(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const SizedBox(
+          height: 32,
+        ),
+        Center(
+          child: GestureDetector(
+            onTap: () {
+              addimgae();
+              Timer(Duration(seconds: 2), () => set());
+              neenear = 100;
+            },
+            child: CircleAvatar(
+              radius: neenear,
+              backgroundColor: element.main,
+              child: images != null
+                  ? ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.file(
+                        File(images!.path),
+                        width: 200,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  : Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(50)),
+                      width: 102,
+                      height: 102,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.camera_alt,
+                            color: Colors.grey[800],
+                          ),
+                          Text('ຮູບພາບ',
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                              )),
+                        ],
+                      ),
+                    ),
             ),
           ),
         ),
-        const SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "ລາຍລະອຽດ",
-            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
-            filled: true,
-            prefixIcon: const Icon(Icons.description_sharp),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
-              borderSide: BorderSide.none,
-            ),
-          ),
-        ),
-        const SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "ລາຄາ",
-            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
-            filled: true,
-            prefixIcon: const Icon(Icons.price_change_sharp),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
-              borderSide: BorderSide.none,
-            ),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        const SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "ລາຄາທືນ",
-            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
-            filled: true,
-            prefixIcon: const Icon(Icons.price_change),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
-              borderSide: BorderSide.none,
-            ),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        const SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "ຈຳນວນ",
-            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
-            filled: true,
-            prefixIcon: const Icon(Icons.add),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
-              borderSide: BorderSide.none,
-            ),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        const SizedBox(height: 10),
-      comboboxcontainer(context),
-        const SizedBox(height: 40),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              primary: element.main,
-              shape: const StadiumBorder(),
-              padding: const EdgeInsets.symmetric(vertical: 16)),
-          onPressed: () {
-            uptostorge( 'dddd','desciption', 10, 10, 10, 'category');
-          },
-          child: const Text(
-            "ບັນທືກ",
-            style: TextStyle(fontSize: 20),
+        const SizedBox(height: 20),
+        Form(
+          key: Key,
+          child: Column(
+            children: [
+              _inputFields(
+                  context,
+                  trye: 'Name',
+                  Keybordtye: TextInputType.text,
+                  hint: 'ຊື່ສິນຄ້າ',
+                  icons: Icons.production_quantity_limits,
+              ),
+              SizedBox(height: 10),
+              _inputFields(
+                context,
+                trye: 'Name',
+                Keybordtye: TextInputType.text,
+                hint: 'ຊື່ສິນຄ້າ',
+                icons: Icons.production_quantity_limits,
+              ),
+              SizedBox(height: 10),
+              _inputFields(
+                context,
+                trye: 'Name',
+                Keybordtye: TextInputType.text,
+                hint: 'ຊື່ສິນຄ້າ',
+                icons: Icons.production_quantity_limits,
+              ),
+              SizedBox(height: 10),
+              _inputFields(
+                context,
+                trye: 'Name',
+                Keybordtye: TextInputType.text,
+                hint: 'ຊື່ສິນຄ້າ',
+                icons: Icons.production_quantity_limits,
+              ),
+              SizedBox(height: 10),
+              comboboxcontainer(context),
+              SizedBox(height: 10),
+              elevatedButton(),
+            ],
           ),
         ),
       ],
+    );
+  }
+}
+
+_inputFields(context, {String? trye, TextInputType? Keybordtye, String? hint,IconData? icons }) {
+  return TextFormField(
+    decoration: InputDecoration(
+      hintText: hint,
+      fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+      filled: true,
+      prefixIcon: Icon(icons),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(18),
+        borderSide: BorderSide.none,
+      ),
     ),
+    keyboardType: Keybordtye,
+    validator: (v) {
+      var tye = trye;
+       if(v!.isEmpty){return 'ກະລຸໃສ່້ຂໍມູນ';}else if(v.length<3){return 'ຊື່ສິນຄ້າສັ້ນໄປ';}
+      switch (tye) {
+        case "Name":
+          {
+
+          }
+          break;
+        case "B":
+          {
+
+          }
+          break;
+
+        case "C":
+          {
+
+          }
+          break;
+
+        case "D":
+          {
+
+          }
+          break;
+
+        default:
+          {
+          }
+          break;
+      }
+    },
+    onTap: () {},
   );
 }
+
 Container comboboxcontainer(context) {
   return Container(
       decoration: BoxDecoration(
           color: Theme.of(context).primaryColor.withOpacity(0.1),
           borderRadius: BorderRadius.circular(18)),
       child: Column(
-        children: [dropdownButton()],
+        children: [
+          dropdownButton(),
+        ],
       ));
 }
 
@@ -215,8 +243,7 @@ DropdownButton<String> dropdownButton() {
       ),
       // icon: const Icon(Icons.keyboard_arrow_down),
       items: EmployeeData.positoin
-          .map((e) =>
-          DropdownMenuItem(
+          .map((e) => DropdownMenuItem(
               value: e,
               child: Padding(
                 padding: const EdgeInsets.only(left: 20.0),
